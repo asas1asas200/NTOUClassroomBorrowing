61d6046fd4c4cb9cd3b412971d337a898b0953df
asas1asas200 <asas1asas200@gmail.com>
2022-11-07 01:03:36 +0800

    feat(admin/classroom): add frontend page

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -8,7 +8,28 @@ const auth = require("../middleware/auth");
 router.use(auth.admin);
 
 router.get("/", function (req, res, next) {
-  res.render("admin/home", { title: "Admin" });
+  res.render("admin/home", { title: "Admin", page: null });
+});
+
+router.get("/classroom", function (req, res, next) {
+  //TODO: communicate with database
+  let fakeData = {
+    buildings: {
+      電機二館: {
+        一樓: ["101", "105"],
+        二樓: ["201", "203"],
+        三樓: ["301", "303"],
+      },
+      電綜大樓: {
+        一樓: ["101", "105"],
+      },
+    },
+  };
+  res.render("admin/home", {
+    title: "Admin",
+    page: "classroom",
+    data: fakeData,
+  });
 });
 
 module.exports = router;
