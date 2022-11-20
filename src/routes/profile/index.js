f6c43dc59b546ef8f4849ae6cfe22219af1ee689
Leo Spin <leochang658@gmail.com>
2022-11-20 10:02:59 +0800

    edit profile - edit username, email, phone

--- src/routes/profile/index.js
+++ src/routes/profile/index.js
@@ -2,11 +2,27 @@ var express = require("express");
 var router = express.Router();
 
 const auth = require("../../middleware/auth");
+const User = require("../../models/user");
 
 router.use(auth.loginedUser);
 
-router.get("/", function (req, res, next) {
-  res.render("profile/home", { title: "profile edit", page: null });
+router.get("/", async function (req, res, next) {
+  let users = await User.find({ verified: false }).lean();
+  res.render("profile/home", { title: "profile edit", data: users });
+});
+
+router.post("/:id", async function (req, res, next) {
+  try {
+    let user = await User.findOne({ id: req.params.id }).lean();
+    user.username = req.body.data.username;
+    user.email = req.body.data.email;
+    user.phone = req.body.data.phone;
+    await User.updateOne({ id: req.params.id }, user);
+
+    res.status(200).send("OK");
+  } catch (e) {
+    res.status(500).send(e);
+  }
 });
 
 module.exports = router;
