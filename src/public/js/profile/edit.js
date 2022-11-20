f67777e814eaa3143e34120422cb1356003d4abb
Leo Spin <leochang658@gmail.com>
2022-11-20 17:25:05 +0800

    delete currentUser in src/routes/profile/index.js, check user id and add views/js/profile/edit.js

---
+++ src/public/js/profile/edit.js
@@ -0,0 +1,56 @@
+const url = `${window.location.protocol}//${window.location.host}/profile`;
+const csrfToken = document.getElementById("csrfToken").value;
+let passwordBlock = document.getElementById("passwordEditBlock");
+function editPassword() {
+  //remove the row class to avoid additional margin and border
+  passwordBlock.setAttribute("class", "mb-3");
+  passwordBlock.innerHTML = `
+    <div class="mb-3 row">
+        <div class="col-3">
+          目前的密碼:
+        </div>
+        <div class="col-3">
+          <input type="password" class="form-control" id="originPassword" value="">
+        </div>
+    </div>
+    <div class="mb-3 row">
+        <div class="col-3">
+          新密碼:
+        </div>
+        <div class="col-3">
+          <input type="password" class="form-control" id="newPassword" value="">
+        </div>
+    </div>
+    <div class="mb-3 row">
+        <div class="col-3">
+        再次輸入新密碼:
+        </div>
+        <div class="col-3">
+          <input type="password" class="form-control" id="newPasswordCheck" value="">
+        </div>
+    </div>`;
+}
+function editProfile(userID) {
+  //if (!userID) userID = window.sessionStorage.getItem("userID");
+  console.log("userID: ", userID);
+  let newUsername = document.getElementById("username").value;
+  let newEmail = document.getElementById("email").value;
+  let newPhone = document.getElementById("phone").value;
+
+  axios
+    .post(url + `/${userID}`, {
+      _csrf: csrfToken,
+      data: {
+        id: userID,
+        username: newUsername,
+        email: newEmail,
+        phone: newPhone,
+      },
+    })
+    .then((res) => {
+      window.location.reload();
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
--- src/routes/profile/index.js
+++ src/routes/profile/index.js
@@ -7,19 +7,22 @@ const User = require("../../models/user");
 router.use(auth.loginedUser);
 
 router.get("/", async function (req, res, next) {
-  let currentUser = req.user;
-  res.render("profile/home", { title: "profile edit", data: currentUser });
+  res.render("profile/home", { title: "profile edit", data: null });
 });
 
 router.post("/:id", async function (req, res, next) {
   try {
-    let user = await User.findOne({ id: req.params.id }).lean();
-    user.username = req.body.data.username;
-    user.email = req.body.data.email;
-    user.phone = req.body.data.phone;
-    await User.updateOne({ id: req.params.id }, user);
+    let receivedUserId = req.body.data.id;
+    console.log("received id: " + receivedUserId);
+    if(receivedUserId == req.user.id){
+      let user = await User.findOne({ id: req.params.id }).lean();
+      user.username = req.body.data.username;
+      user.email = req.body.data.email;
+      user.phone = req.body.data.phone;
+      await User.updateOne({ id: req.params.id }, user);
 
-    res.status(200).send("OK");
+      res.status(200).send("OK");
+    }
   } catch (e) {
     res.status(500).send(e);
   }
