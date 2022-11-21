df7b5faea53813cfb0ecaa4dd2017bac77a0a59b
asas1asas200 <asas1asas200@gmail.com>
2022-11-21 15:39:42 +0800

    feat(model/lesson): add lesson schema

---
+++ src/models/lesson.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const lessonSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  teacher: {
+    type: String,
+    required: true,
+  },
+  borrowerID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  description: {
+    type: String,
+  },
+  fixed: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Lesson", lessonSchema);
