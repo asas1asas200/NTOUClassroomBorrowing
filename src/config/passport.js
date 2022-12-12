ddb6149f98c32545a03f0caf32615fa9a8b04e67
lin121291 <4jp33f9e@gmail.com>
2022-12-12 18:16:27 +0800

    rewrite passport use

--- src/config/passport.js
+++ src/config/passport.js
@@ -5,28 +5,39 @@ module.exports = (app) => {
   app.use(passport.initialize());
   app.use(passport.session());
   passport.use(
-    new LocalStrategy({ usernameField: "id" }, (id, password, done) => {
-      User.findOne({ id })
+    new LocalStrategy(function (id, password, done) {
+      console.log(id + " ; " + password);
+      User.findOne({ id: id })
         .then((user) => {
           if (!user) {
             return done(null, false, { message: "Incorrect username." });
           }
-          if (user.password !== password) {
-            return done(null, false, { message: "Incorrect password." });
-          }
-          return done(null, user);
+          User.comparePassword(
+            password,
+            user.password,
+            function (err, isMatch) {
+              if (err) throw err;
+              if (isMatch) {
+                return done(null, user);
+              } else {
+                return done(null, false, { message: "Invalid password" });
+              }
+            }
+          );
         })
         .catch((err) => {
           done(err, false);
         });
     })
   );
-  passport.serializeUser((user, done) => {
+
+  passport.serializeUser(function (user, done) {
     done(null, user.id);
   });
-  passport.deserializeUser((id, done) => {
-    User.findOne({ id })
-      .then((user) => done(null, user))
-      .catch((err) => done(err, null));
+
+  passport.deserializeUser(function (id, done) {
+    User.getUserById(id, function (err, user) {
+      done(err, user);
+    });
   });
 };
